cd4dcca61cd675111986daa759a00629dcd0ed62
yuvit256 <117669404+yuvit256@users.noreply.github.com>
2022-12-28 15:57:39 +0200

    Add files via upload
    Uploads the tests

---
+++ Test_ConcreteMember.java
@@ -0,0 +1,5 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.logging.Logger;
+import org.junit.platform.commons.logging.LoggerFactory;
---
+++ Test_GroupAdmin.java
@@ -0,0 +1,7 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.logging.Logger;
+import org.junit.platform.commons.logging.LoggerFactory;
+public class Test_GroupAdmin {
+}
